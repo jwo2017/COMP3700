6a893adbef9c8c4fd58e23d3a9d6bed3acd92d96
Jonathan <jwo.2017@gmail.com>
2019-08-23 12:21:17 -0500

    Created findMax and findMin methods.

--- src/main/java/Finder.java
+++ src/main/java/Finder.java
@@ -1,2 +1,28 @@
 public class Finder {
+    public static void main(String[] args) {
+
+
+    }
+
+    public int findMax(int[] intArray) {
+        int max = intArray[0];
+        for (int elem : intArray) {
+            if (elem > max) {
+                max = elem;
+            }
+        }
+        return max;
+    }
+
+    public int findMin(int[] intArray) {
+        int min = intArray[0];
+        for (int elem : intArray) {
+            if (elem < min) {
+                min = elem;
+            }
+        }
+        return min;
+    }
+
+
 }
